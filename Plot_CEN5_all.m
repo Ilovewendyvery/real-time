7abe6669bc61aad6d86de47b09d8ac4e20840194
kimberly_shang <shang_l@126.com>
2025-08-18 16:21:02 +1000

    Signed-off-by: kimberly_shang <shang_l@126.com>

--- Plot_CEN5_all.m
+++ Plot_CEN5_all.m
@@ -10,11 +10,14 @@ colors = {'b', 'g', 'r', 'c', 'm', 'y', 'k'};
 time=0.5:0.5:obj.Data.T/2;
 figure;
 hold on;
-
+xdata=time;
 for i=1:1
     h1(i) = plot(time,(AA(i,:)*[obj.PevT;obj.PbuyT]), ...
         'LineStyle', linestyle{1}, 'Color', colors{i}, 'LineWidth', 2);
+    
 end 
+ydata1=(AA(i,:)*[obj.PevT;obj.PbuyT]);
+
 
 
 B=obj.Data.B_feeder; 
@@ -25,6 +28,7 @@ for i=1:1
     h2(i) = plot(time,(AA(i,:)*[obj.PevT;obj.PbuyT]), ...
         'LineStyle', linestyle{2}, 'Color', colors{i}, 'LineWidth', 2);
 end
+ydata2=(AA(i,:)*[obj.PevT;obj.PbuyT]);
 
 % 只保留蓝色曲线的图例，且不带数字
 % 蓝色曲线是 colors{1}，即 i=1 的曲线
@@ -35,3 +39,6 @@ xlabel('Time (h)')
 ylabel('Total gird power(kW) ')
 title('Powers of feeders with considering the capacity constraints for 5 feeders for Centralized optimization')
 box on
+
+data=[xdata',ydata1',ydata2'];
+writematrix(data, 'CEN5_all.csv');
--- Plot_CEN6_feeder.m
+++ Plot_CEN6_feeder.m
@@ -13,7 +13,7 @@ if isupdata
     save('result/ADMM100_noRestrain.mat','obj')
 end
  
- TT='result/ADMM100.mat'; 
+ TT='result/ADMM100_S150.mat'; 
  
 load(TT,'obj')
 AA=obj.Data.U_feeder; 
@@ -21,10 +21,13 @@ colors = {'b', 'g', 'r', 'c', 'm', 'y', 'k'};
 time=0.5:0.5:obj.Data.T/2;
 figure;
 hold on;
+xdata=time;
+ydata=zeros(10,48);idx=0;
 
 for i=1:obj.Data.number_of_feeder
     h1(i) = plot(time,(AA(i,:)*[obj.PevT;obj.PbuyT])/obj.Data.B_feeder(i), ...
         '-o', 'Color', colors{i}, 'LineWidth', 0.5);
+    ydata(idx+i,:)=(AA(i,:)*[obj.PevT;obj.PbuyT])/obj.Data.B_feeder(i);
 end 
 
 
@@ -32,9 +35,11 @@ B=obj.Data.B_feeder;
 TT='result/ADMM100_noRestrain.mat';
 load(TT,'obj') 
 time=0.5:0.5:obj.Data.T/2;  
+idx=5;
 for i=1:obj.Data.number_of_feeder
     h2(i) = plot(time,(AA(i,:)*[obj.PevT;obj.PbuyT])/B(i), ...
         '--', 'Color', colors{i}, 'LineWidth', 0.5);
+    ydata(idx+i,:)=(AA(i,:)*[obj.PevT;obj.PbuyT])/B(i);
 end
 
 % 只保留蓝色曲线的图例，且不带数字
@@ -46,3 +51,5 @@ xlabel('Time (h)')
 ylabel('Percentage')
 %title('Powers percentage of feeders with  considering the capacity constraints for 5 feeders for Centralized optimization')
 box on
+data=[xdata',ydata'];
+writematrix(data, 'CENfeeder.csv');
